25522ec4a0fc787371c0a10c94af8d2c2a1842b3
Renan Rabelo <renanrabelo@meuprojeto.com>
2025-10-14 08:53:56 -0300

    exercicio 2

---
+++ 2-Fundamentos/Exercicio2.py
@@ -0,0 +1,22 @@
+"""
+Escreva um programa que
+ imprima na tela,
+cada um em uma linha,
+um número inteiro, 
+um número decimal (float) 
+e um texto (string).
+
+Formato esperado da saída:
+42
+3.14
+Python
+
+"""
+
+idadeDeRoberto = 42
+valorDePi = 3.14
+linguagemDeProgramacao = "Python"
+
+print(idadeDeRoberto)
+print(valorDePi)
+print(linguagemDeProgramacao)
